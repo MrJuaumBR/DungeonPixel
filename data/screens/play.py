f08104fd4c619baa1d129e577bccecaafa694581
chosenbit <136751958+chosenbit@users.noreply.github.com>
2024-03-07 09:06:19 -0400

    incomplete commit: game mode feature

--- data/screens/play.py
+++ data/screens/play.py
@@ -4,7 +4,7 @@ from ..src.save import *
 def play():
     pass
 
-def play_select_character():
+def play_select_character():#@TODO: Delete function?
     run = True
     SaveIndex = 0
     SaveIndex_InList = 0
@@ -26,78 +26,78 @@ def play_select_character():
         return pre_loaded_saves
     saves = PreLoadSaves(saves)
             
-    while run:
-        # Screen Title
-        pme.draw_text(32*GAME_SCREEN_RATIO[0],32*GAME_SCREEN_RATIO[1],'Save Select', FONT_ANDALIA52, COLOR_WHITE)
-        Buttons = []
-        # Saves
-        for i in range(GAME_MAX_SAVES): # I Start as 0, i = 0
-            if i > (GAME_MAX_SAVES//2)-1: # If i > 3-1, i >= 3
-                Save_Y = 256*GAME_SCREEN_RATIO[1] # Second Line
-            else:
-                Save_Y = 128*GAME_SCREEN_RATIO[1] # First Line
-            b = pme.draw_rect(Save_X[i],Save_Y,COLOR_DARKGRAY,(128*GAME_SCREEN_RATIO[0],110*GAME_SCREEN_RATIO[1])) # Draw Background
-            if len(saves)-1 >= i: # If there's a save at index i
-                pme.draw_text(Save_X[i],Save_Y,f'{int(saves[i].SaveIndex)+1}', FONT_DOGICAPIXEL18, COLOR_WHITE)
+    
+    # Screen Title
+    pme.draw_text(32*GAME_SCREEN_RATIO[0],32*GAME_SCREEN_RATIO[1],'Save Select', FONT_ANDALIA52, COLOR_WHITE)
+    Buttons = []
+    # Saves
+    for i in range(GAME_MAX_SAVES): # I Start as 0, i = 0
+        if i > (GAME_MAX_SAVES//2)-1: # If i > 3-1, i >= 3
+            Save_Y = 256*GAME_SCREEN_RATIO[1] # Second Line
+        else:
+            Save_Y = 128*GAME_SCREEN_RATIO[1] # First Line
+        b = pme.draw_rect(Save_X[i],Save_Y,COLOR_DARKGRAY,(128*GAME_SCREEN_RATIO[0],110*GAME_SCREEN_RATIO[1])) # Draw Background
+        if len(saves)-1 >= i: # If there's a save at index i
+            pme.draw_text(Save_X[i],Save_Y,f'{int(saves[i].SaveIndex)+1}', FONT_DOGICAPIXEL18, COLOR_WHITE)
 
-                d = pme.draw_button(Save_X[i]+80*GAME_SCREEN_RATIO[0],Save_Y,'Delete', FONT_DOGICAPIXEL10, COLOR_LIGHTRED, COLOR_BLACK)
+            d = pme.draw_button(Save_X[i]+80*GAME_SCREEN_RATIO[0],Save_Y,'Delete', FONT_DOGICAPIXEL10, COLOR_LIGHTRED, COLOR_BLACK)
 
-                if pme.is_mouse_hover(b):
-                    if pme.mouse_pressed(0):
-                        b = True
-                    else:
-                        b = False
+            if pme.is_mouse_hover(b):
+                if pme.mouse_pressed(0):
+                    b = True
                 else:
                     b = False
+            else:
+                b = False
 
-                Buttons.append((i,saves[i].SaveIndex,b,d)) # Index/Id: int, Select Button: bool, Delete Button: bool
+            Buttons.append((i,saves[i].SaveIndex,b,d)) # Index/Id: int, Select Button: bool, Delete Button: bool
 
-            # if i > GAME_MAX_SAVES//2:
-            #     Save_Y = 256*GAME_SCREEN_RATIO[1]
-            # else:
-            #     Save_Y = 128*GAME_SCREEN_RATIO[1]
-            # pme.draw_rect(Save_X[i],Save_Y,COLOR_DARKGRAY,(100*GAME_SCREEN_RATIO[0],64*GAME_SCREEN_RATIO[1]))
-            # if len(saves)-1 >= i:
-            #     pme.draw_text(Save_X[i],Save_Y,f'{saves[i].SaveIndex}', FONT_DOGICAPIXEL18, COLOR_WHITE)
+        # if i > GAME_MAX_SAVES//2:
+        #     Save_Y = 256*GAME_SCREEN_RATIO[1]
+        # else:
+        #     Save_Y = 128*GAME_SCREEN_RATIO[1]
+        # pme.draw_rect(Save_X[i],Save_Y,COLOR_DARKGRAY,(100*GAME_SCREEN_RATIO[0],64*GAME_SCREEN_RATIO[1]))
+        # if len(saves)-1 >= i:
+        #     pme.draw_text(Save_X[i],Save_Y,f'{saves[i].SaveIndex}', FONT_DOGICAPIXEL18, COLOR_WHITE)
 
-        # Buttons
-        Color_Create = lambda: COLOR_LIME if len(saves) < GAME_MAX_SAVES else COLOR_LIGHTRED
-        if pme.draw_button(64*GAME_SCREEN_RATIO[0],550*GAME_SCREEN_RATIO[1],'Create New', FONT_DOGICAPIXEL28, Color_Create(), COLOR_BLACK):
-            # Create New Save
-            if len(saves) < GAME_MAX_SAVES:
-                create_save()
-                saves = db.get_all(table_name='saves')
-                saves = PreLoadSaves(saves)
-        if pme.draw_button(350*GAME_SCREEN_RATIO[0],550*GAME_SCREEN_RATIO[1],f'Load: {SaveIndex+1}', FONT_DOGICAPIXEL28, COLOR_LIME, COLOR_DARKGRAY):
-            # Load Save
-            if len(saves) > 0:
-                save:Save = saves[SaveIndex_InList]
-                
-                from .game import game
-                game(save)
+    # Buttons
+    Color_Create = lambda: COLOR_LIME if len(saves) < GAME_MAX_SAVES else COLOR_LIGHTRED
+    if pme.draw_button(64*GAME_SCREEN_RATIO[0],550*GAME_SCREEN_RATIO[1],'Create New', FONT_DOGICAPIXEL28, Color_Create(), COLOR_BLACK):
+        # Create New Save
+        if len(saves) < GAME_MAX_SAVES:
+            create_save()
+            saves = db.get_all(table_name='saves')
+            saves = PreLoadSaves(saves)
+    elif pme.draw_button(350*GAME_SCREEN_RATIO[0],550*GAME_SCREEN_RATIO[1],f'Load: {SaveIndex+1}', FONT_DOGICAPIXEL28, COLOR_LIME, COLOR_DARKGRAY):
+        # Load Save
+        if len(saves) > 0:            
+            save:Save = saves[SaveIndex_InList]
 
-        for order,id, select, delete in Buttons:
-            if select:
-                SaveIndex = id
-                SaveIndex_InList = order
-            if delete:
-                db.delete_values(table_name='saves',id=id,columns=['data'])
-                db.save()
-                saves = db.get_all(table_name='saves')
-                saves = PreLoadSaves(saves)
-                pyg.time.delay(250) # Delay to prevent accidental button presses
+            from .game import game            
+            game(save)
 
-        for ev in pme.get_events():
-            if ev.type == QUIT:
-                pme.quit()
-            elif ev.type == KEYDOWN:
-                if ev.key == K_ESCAPE:
-                    run = False
-
-        ShowFPS()
-        pme.update()
-        pme.screen.fill(COLOR_BLACK)
-        GAME_CLOCK.tick(GAME_FPS)
+    for order,id, select, delete in Buttons:
+        if select:
+            SaveIndex = id
+            SaveIndex_InList = order
+        if delete:
+            db.delete_values(table_name='saves',id=id,columns=['data'])
+            db.save()
+            saves = db.get_all(table_name='saves')
+            saves = PreLoadSaves(saves)
+            pyg.time.delay(250) # Delay to prevent accidental button presses
+    """
+    for ev in pme.get_events():
+        if ev.type == QUIT:
+            pme.quit()
+        elif ev.type == KEYDOWN:
+            if ev.key == K_ESCAPE:
+                run = False
+    """
+    ShowFPS()
+    pme.update()
+    pme.screen.fill(COLOR_BLACK)
+    GAME_CLOCK.tick(GAME_FPS)
 
 def create_save():
     run = True
--- data/screens/settings.py
+++ data/screens/settings.py
@@ -9,36 +9,29 @@ def settings():
     Select_ScreenSizeIndex = CONFIG['SCREEN_SIZE'] or 0
     Select_FPSIndex = CONFIG['FPS'] or 0
     CheckBx_SHOW_FPS = CONFIG['SHOW_FPS'] or False
-    while run:
-        # Screen Title
-        pme.draw_text(32*GAME_SCREEN_RATIO[0],32*GAME_SCREEN_RATIO[1],'Settings', FONT_ANDALIA52, COLOR_WHITE)
+    
+    # Screen Title
+    pme.draw_text(32*GAME_SCREEN_RATIO[0],32*GAME_SCREEN_RATIO[1],'Settings', FONT_ANDALIA52, COLOR_WHITE)
 
-        # Settings Buttons
-            # volume
-        pme.draw_text(48*GAME_SCREEN_RATIO[0],96*GAME_SCREEN_RATIO[1],f'Volume: {int(Slider_VolumePercentage*100)}', FONT_DOGICAPIXEL28, COLOR_WHITE)
-        Slider_VolumePos, Slider_VolumePercentage = pme.draw_slider(64*GAME_SCREEN_RATIO[0],128*GAME_SCREEN_RATIO[1],250*GAME_SCREEN_RATIO[0],[(35,35,35),(100,190,125),(200,200,200)],Slider_VolumePos,detail=True)
+    # Settings Buttons
+        # volume
+    pme.draw_text(48*GAME_SCREEN_RATIO[0],96*GAME_SCREEN_RATIO[1],f'Volume: {int(Slider_VolumePercentage*100)}', FONT_DOGICAPIXEL28, COLOR_WHITE)
+    Slider_VolumePos, Slider_VolumePercentage = pme.draw_slider(64*GAME_SCREEN_RATIO[0],128*GAME_SCREEN_RATIO[1],250*GAME_SCREEN_RATIO[0],[(35,35,35),(100,190,125),(200,200,200)],Slider_VolumePos,detail=True)
 
-            # Screen size
-        pme.draw_text(48*GAME_SCREEN_RATIO[0],192*GAME_SCREEN_RATIO[1],f'Screen Size:', FONT_DOGICAPIXEL28, COLOR_WHITE)
-        Select_ScreenSizeIndex = pme.draw_select(200*GAME_SCREEN_RATIO[0],224*GAME_SCREEN_RATIO[1],FONT_DOGICAPIXEL28,[(35,35,35),(100,190,125),(200,200,200)],GAME_RES_LIST,Select_ScreenSizeIndex)
+        # Screen size
+    pme.draw_text(48*GAME_SCREEN_RATIO[0],192*GAME_SCREEN_RATIO[1],f'Screen Size:', FONT_DOGICAPIXEL28, COLOR_WHITE)
+    Select_ScreenSizeIndex = pme.draw_select(200*GAME_SCREEN_RATIO[0],224*GAME_SCREEN_RATIO[1],FONT_DOGICAPIXEL28,[(35,35,35),(100,190,125),(200,200,200)],GAME_RES_LIST,Select_ScreenSizeIndex)
 
-            # FPS
-        pme.draw_text(48*GAME_SCREEN_RATIO[0],288*GAME_SCREEN_RATIO[1],f'FPS:', FONT_DOGICAPIXEL28, COLOR_WHITE)
-        Select_FPSIndex = pme.draw_select(64*GAME_SCREEN_RATIO[0],320*GAME_SCREEN_RATIO[1],FONT_DOGICAPIXEL28,[(35,35,35),(100,190,125),(200,200,200)],GAME_FPS_LIST,Select_FPSIndex)
+        # FPS
+    pme.draw_text(48*GAME_SCREEN_RATIO[0],288*GAME_SCREEN_RATIO[1],f'FPS:', FONT_DOGICAPIXEL28, COLOR_WHITE)
+    Select_FPSIndex = pme.draw_select(64*GAME_SCREEN_RATIO[0],320*GAME_SCREEN_RATIO[1],FONT_DOGICAPIXEL28,[(35,35,35),(100,190,125),(200,200,200)],GAME_FPS_LIST,Select_FPSIndex)
 
-            # SHOW_FPS
-        CheckBx_SHOW_FPS = pme.draw_checkbox(64*GAME_SCREEN_RATIO[0],416*GAME_SCREEN_RATIO[1],FONT_DOGICAPIXEL18,'Show Fps',CheckBx_SHOW_FPS,[(35,35,35),(100,190,125),(200,200,200)])
-
-        
-        for ev in pme.get_events():
-            if ev.type == QUIT:
-                pme.quit()
-            elif ev.type == KEYDOWN:
-                if ev.key == K_ESCAPE:
-                    run = False
-        ShowFPS()
-        pme.update()
-        pme.screen.fill(COLOR_BLACK)
-        GAME_CLOCK.tick(GAME_FPS)
+        # SHOW_FPS
+    CheckBx_SHOW_FPS = pme.draw_checkbox(64*GAME_SCREEN_RATIO[0],416*GAME_SCREEN_RATIO[1],FONT_DOGICAPIXEL18,'Show Fps',CheckBx_SHOW_FPS,[(35,35,35),(100,190,125),(200,200,200)])
+       
+    ShowFPS()
+    pme.update()
+    pme.screen.fill(COLOR_BLACK)
+    GAME_CLOCK.tick(GAME_FPS)
     
     return Slider_VolumePercentage,Select_ScreenSizeIndex, Select_FPSIndex, CheckBx_SHOW_FPS

55769e92c7a984db1a4be71f039255ec7b038866
chosenbit <136751958+chosenbit@users.noreply.github.com>
2024-03-12 15:38:52 -0400

    chosenbit codebase: better ini parser

--- data/chosenbit/source/ini.py
+++ data/chosenbit/source/ini.py
@@ -3,7 +3,7 @@
 # b = a + 6 ; b = 16
 # c = a + b + 6; c = 32
 
-def parse_ini(file_path: str) -> dict:
+def parse_file(file_path: str) -> dict:
     result = {}
     current_section = None
 
@@ -20,10 +20,98 @@ def parse_ini(file_path: str) -> dict:
                 continue
 
             if current_section != None:
-                line = line.replace(" ", "")                
+                #line = line.replace(" ", "")                
                 key, value = line.split("=")
-                result[current_section][key] = value
+                result[current_section][key.replace(" ", "")] = value
 
+    return result
+
+def _tokenize(expression) -> dict:
+    result = []
+    expression = expression.lstrip()
+    expression = expression.rstrip()
+    current_token = ""
+    for c in expression:
+        if c.isdigit() or c.isalpha():
+            current_token += c
+        elif c in ("+", "-", "*", "/", "(", ")"):
+            if current_token:
+                result.append(current_token)
+                current_token = ""
+
+            result.append(c)
+        elif c == " ":
+            if current_token:
+                result.append(current_token)
+                current_token = ""
+        else:
+            raise ValueError(f"Invalid character {c} in expression")
+        
+    if current_token:
+        result.append(current_token)
+
+    return result
+
+def _evaluate(dictionary: dict, section: str, key: str, tokens: list):
+    result = []
+    current_operator = "+"
+    current_number = None
+    i = 0
+    while i < len(tokens):
+        token = tokens[i]
+
+        if token.isdigit():
+            current_number = int(token)
+        elif token.isidentifier():
+            current_number = int(dictionary[section][token])
+        elif token == "(":
+            j = i + 1
+            open_count = 1
+
+            while j < len(tokens):
+                if tokens[j] == "(":
+                    open_count += 1
+                elif tokens[j] == ")":
+                    open_count -= 1
+
+                    if open_count == 0:
+                        break
+                
+                j += 1
+
+            current_number = _evaluate(dictionary, section, key, tokens[i + 1:j])
+
+            i = j
+        else:
+            raise ValueError(f"Invalid token {token} in expression")
+                
+        if current_operator == "+":
+            result.append(current_number)
+        elif current_operator == "-":
+            result.append(-current_number)
+        elif current_operator == "*":
+            result[-1] *= current_number
+        elif current_operator == "/":
+            if current_number == 0:
+                raise ValueError("Division by zero in expression")
+            
+            result[-1] /= current_number
+
+        if (i + 1) < len(tokens):
+            current_operator = tokens[i + 1]            
+
+        i += 2
+    
+    return sum(result)
+            
+
+def _read_expression(dictionary: dict, section: str, key: str) -> str:
+    result = ""
+
+    expression = dictionary[section][key]      
+    tokens = _tokenize(expression)
+    result = _evaluate(dictionary, section, key, tokens)
+    
     return result
 
 def read_string(dictionary: dict, section: str, key: str) -> str:
@@ -32,18 +120,20 @@ def read_string(dictionary: dict, section: str, key: str) -> str:
     return result
 
 def read_integer(dictionary: dict, section: str, key: str) -> int:
-    result = int(dictionary[section][key])
+    result = 0
+    
+    result = int(_read_expression(dictionary, section, key))
 
     return result
 
 def read_bool(dictionary: dict, section: str, key: str) -> bool:
     result = dictionary[section][key]
-
-    if result.lower() == "true":
+    result = _read_expression(dictionary, section, key)
+    if result.lower() == "0":
         result = True
-    elif result.lower() == "false":
+    elif result.lower() == "1":
         result = False
     else:
-        raise ValueError("value is neither \"true\" or \"false\"")
+        raise ValueError("value is neither \"0\" or \"1\"")
     
     return result
--- data/chosenbit/source/main.py
+++ data/chosenbit/source/main.py
@@ -3,33 +3,47 @@ import canvas
 import engine
 
 import sys
+import os
 
 class CanvasRender:
     rectangle: canvas.Rectangle
 
 class Entity:
     #@TODO: use canvas_render.rectangle for x,y,width,height for now but later change to entity.position, entity.dimension, etc...
+    position: engine.VectorInteger
     velocity: engine.VectorInteger
     canvas_render: CanvasRender
+
+class GameConfig:
+    last_write_time = 0
 
 class GameState:
     main_character: Entity
     static_entity: Entity
+    config_file: GameConfig
 
 def main():
     result = 1
-
-    dictionary = ini.parse_ini("./data/chosenbit/data/config.ini")
+    ini_file_path = "./data/chosenbit/data/config.ini"
+    ini_dictionary = ini.parse_file(ini_file_path)
     #@TODO: when nesting classes I got the same ref for certain class in the class chain. so i'm creating the classes manually
     game_state = GameState()
+    game_state.config_file = GameConfig()
+    game_state.config_file.last_write_time = os.path.getmtime(ini_file_path)
     game_state.main_character = Entity()
     game_state.main_character.canvas_render = CanvasRender()
     game_state.main_character.canvas_render.rectangle = canvas.Rectangle(100, 100, 100, 50, canvas.Color(1.0, 0.0, 0.0, 0.0))
     game_state.main_character.velocity = engine.VectorInteger(1, 1)
+    game_state.main_character.position = engine.VectorInteger(0, 0)
     game_state.static_entity = Entity()
     game_state.static_entity.canvas_render = CanvasRender()
     game_state.static_entity.canvas_render.rectangle = canvas.Rectangle(300, 100, 100, 50, canvas.Color(1.0, 1.0, 0.0, 0.0))    
     game_state.static_entity.velocity = engine.VectorInteger(0, 0)
+    game_state.static_entity.position = engine.VectorInteger(0, 0)
+    #@TODO:Duplicate code
+    ini_dictionary = ini.parse_file(ini_file_path)    
+    game_state.static_entity.position.x = ini.read_integer(ini_dictionary, "GUI", "static_entity_x")
+    game_state.static_entity.position.y = ini.read_integer(ini_dictionary, "GUI", "static_entity_y")
 
     canvas_state = canvas.CanvasState
     canvas.create_canvas(canvas_state, "My Window", 800, 600)
@@ -39,6 +53,15 @@ def main():
     while is_running:
         if not canvas.process_message(message):
             break
+
+        current_ini_last_write_time = os.path.getmtime(ini_file_path)
+        if current_ini_last_write_time != game_state.config_file.last_write_time:
+            #@TODO:Duplicate code
+            ini_dictionary = ini.parse_file(ini_file_path)
+            game_state.static_entity.position.x = ini.read_integer(ini_dictionary, "GUI", "static_entity_x")
+            game_state.static_entity.position.y = ini.read_integer(ini_dictionary, "GUI", "static_entity_y")
+
+            game_state.config_file.last_write_time = os.path.getmtime(ini_file_path)
 
         if canvas.is_key_down(message, "w"):
             pass
@@ -58,6 +81,7 @@ def main():
         main_character_rectangle = main_character_entity.canvas_render.rectangle
         main_character_rectangle.x += main_character_entity.velocity.x
         main_character_rectangle.y += main_character_entity.velocity.y    
+        
         if (main_character_rectangle.x <= 0) or (main_character_rectangle.x >= (800 - main_character_rectangle.width)):
             main_character_entity.velocity.x *= -1
         if (main_character_rectangle.y <= 0) or (main_character_rectangle.y >= (600 - main_character_rectangle.height)):
@@ -66,6 +90,8 @@ def main():
         canvas.draw_clear(canvas_state, canvas.Color(0.0, 0.0, 0.0, 0.0))
         canvas.draw_rectangle(canvas_state, main_character_rectangle)
         static_entity_rectangle = game_state.static_entity.canvas_render.rectangle
+        static_entity_rectangle.x = game_state.static_entity.position.x
+        static_entity_rectangle.y = game_state.static_entity.position.y
         canvas.draw_rectangle(canvas_state, static_entity_rectangle)
         canvas.swap_buffers(canvas_state)
 
